c8c64088ffa721f4a385b0209cf7c73e24fe911a
hkaranjule77 <hkaranjule77@gmail.com>
2022-03-02 16:58:12 +0530

    Spring project added

---
+++ mobile-vaccination-algo/src/main/java/com/aghs/mobilevaccinationalgo/MobileVaccinationAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.aghs.mobilevaccinationalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileVaccinationAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileVaccinationAlgorithmApplication.class, args);
+	}
+
+}
---
+++ mobile-vaccination-algo/src/test/java/com/aghs/mobilevaccinationalgo/MobileVaccinationAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aghs.mobilevaccinationalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileVaccinationAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
